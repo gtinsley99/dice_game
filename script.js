f91656ea563219c3537ba907a2821ca5d687a3d2
George Tinsley <george8s@live.co.uk>
2023-08-16 18:51:52 +0100

    added background and fixed dice placement

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const menuDiv = document.getElementById("menuDiv");
 const backMenu = document.getElementById("backMenu");
 const roll = document.getElementById("roll");
 const dice = document.getElementById("dice1")
+const titleSection = document.getElementById("titleSection");
+const topSection = document.getElementById("topSection");
 
 // one player consts
 const gameBox1 = document.getElementById("gameBox1");
@@ -15,6 +17,7 @@ const rollTally = document.getElementById("rollTally");
 // click on one player game in menu
 onePlayer.addEventListener("click", () => {
     title.textContent = "One Player";
+    titleSection.style.marginBottom = "4%";
     playerSelect.style.display = "none";
     menuDiv.style.display = "block";
     gameBox1.style.display = "flex";
@@ -31,10 +34,11 @@ backMenu.addEventListener("click",() => {
     rollTally.textContent = 0;
 })
 
-// roll number generator
+// for score and roll tally
 let score = 0;
 let rolls = 0;
 
+// function for when roll a 1
 lostPopUp = () => {
     title.textContent = "You lost!";
         roll.textContent = "Play again";
@@ -42,6 +46,7 @@ lostPopUp = () => {
         rolls = 0;
 };
 
+// function for when score >= 20
 winPopUp = () => {
     title.textContent = "You Won!";
     score = 0;
@@ -49,13 +54,16 @@ winPopUp = () => {
     roll.textContent = "Play again";
 };
 
+// function for when roll 2-6
 scoreUp = () => {
     scoreTally.textContent = score;
     rollTally.textContent = rolls;
 };
 
+// dice roll core code
 roll.addEventListener("click", () => {
     let randNum = (Math.ceil(Math.random() * 6));
+    // dice animation to number rolled
     if (roll.textContent != "Play again")
         for (let i = 1; i <= 6; i++) {
             dice.classList.remove(`show-${i}`);
@@ -63,13 +71,16 @@ roll.addEventListener("click", () => {
             dice.classList.add(`show-${i}`);
             }
         }
+    // reset score and roll count, play again button
     if (roll.textContent == "Play again"){
         roll.textContent = "Roll";
         title.textContent = "One Player";
         scoreTally.textContent = 0;
         rollTally.textContent = 0;
+    // when lose (roll a 1)
     } else if (randNum === 1){
         setTimeout(lostPopUp, 1000);
+    // when roll 2-6 
     } else{
         score = score + randNum;
         rolls = rolls + 1;
