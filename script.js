f60d6d9563605ea6685b2757cffc3ed448c939cc
George Tinsley <george8s@live.co.uk>
2023-08-16 11:57:41 +0100

    added score and roll divs, fixed theire layout

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ const playerSelect = document.getElementById("playerSelect");
 const title = document.getElementById("title");
 const menuDiv = document.getElementById("menuDiv");
 const backMenu = document.getElementById("backMenu");
+const roll = document.getElementById("roll");
 
 // one player consts
 const oneTitle = document.getElementById("oneTitle");
+const gameBox1 = document.getElementById("gameBox1");
+const score = document.getElementById("score");
+const rolls = document.getElementById("rolls");
+const wins = document.getElementById("wins");
 
 // click on one player game in menu
 onePlayer.addEventListener("click", () => {
     title.textContent = "One Player";
     playerSelect.style.display = "none";
     menuDiv.style.display = "flex";
+    gameBox1.style.display = "flex";
 })
 
 // click back to main menu
@@ -21,3 +27,11 @@ backMenu.addEventListener("click",() => {
     playerSelect.style.display = "flex";
     menuDiv.style.display = "none";
 })
+
+// roll number generator
+// roll.addEventListener("click", () => {
+//     let randNum = [];
+//     randNum.push(Math.ceil(Math.random() * 5));
+//     score = `score : ${randNum + }`
+// })
+
